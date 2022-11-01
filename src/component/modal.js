c73195df40ada6f73614a611a450ac1de1d0c7ad
badr ghanbi <bghanbi50@gmail.com>
2022-11-01 13:48:08 +0100

    attache event handler to our first react app

---
+++ src/component/modal.js
@@ -0,0 +1,12 @@
+function Modal(props){
+   return (
+    <div className="modal">
+        <p>Are you sure ?</p>
+        <button onClick={props.onCancel} className="btn btn--alt">Cancel</button>
+        <button onClick={props.onCancel} className="btn">Confirm</button>
+    </div>
+   )
+}
+
+
+export default Modal;
